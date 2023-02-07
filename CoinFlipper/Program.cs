b6b90ef7b9014e8d762cc8de991e66b5c8e741eb
Uzi-London <75344303+Uzi-London@users.noreply.github.com>
2023-02-07 16:22:37 -0500

    Added comments to HotOrCold Fixed typo in CoinFlipper

--- CoinFlipper/Program.cs
+++ CoinFlipper/Program.cs
@@ -7,7 +7,7 @@ namespace CoinFlipper {
 
             string[] coinFaces = {"Heads", "Tails"}; //sets up an array containing the two faces of a coin
 
-            var random = new Random(); //new Random object which will be used to generate our pseudorandomness
+            var random = new Random(); //new Random object which will be used to generate our pseudo-randomness
             string result = coinFaces[random.Next(coinFaces.Length)]; //get a 'random' number between the length of our coinFaces array (between 0 and 1) and use it to get the indexed value in the array.
 
             Console.WriteLine($"Your coin was flipped, it was: {result}"); //print our result.
--- HotOrCold/Program.cs
+++ HotOrCold/Program.cs
@@ -3,26 +3,22 @@
 namespace HotOrCold {
         public class HOC {
             public static void Main(string[] args) {
-                var random = new Random();
+                var random = new Random(); //new Random object which will be used to generate our pseudo-randomness
                 int target = random.Next(21); //generate a random number between 0 and 20
-
-                Console.WriteLine(target);
                 
-                int guess;
-                
-                Console.WriteLine("Please guess a number between 0 and 20: ");
-                guess = int.Parse(Console.ReadLine());
+                Console.WriteLine("Please guess a number between 0 and 20: "); //ask the user for input
+                int guess = int.Parse(Console.ReadLine()); //parse the user's first input from string to int
 
-                while (guess != target) {
-                    if (guess > target) {
+                while (guess != target) { //loop through this code while the user's guess is different from the target
+                    if (guess > target) { //let the user know if their guess was too high and ask them to try again
                         Console.WriteLine("OOPS, That was too high! Try again: ");
-                    }else {
+                    }else { //let the user know if their guess was too low and ask them to try again
                         Console.WriteLine("Oh no, too low! Try again: ");
                     }
 
-                    guess = int.Parse(Console.ReadLine());
+                    guess = int.Parse(Console.ReadLine()); //grab the user's new guess
                 }
-                Console.WriteLine($"Congratulations, you guessed it! The number was: {target}. Thanks for playing.");     
+                Console.WriteLine($"Congratulations, you guessed it! The number was: {target}. Thanks for playing."); //this message is displayed once the loop is broken, which means the user has guessed the number correctly    
         }
     }
 }
