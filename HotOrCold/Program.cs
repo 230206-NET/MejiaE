e3433088eb2259e92e6b1ae5ca96d73cfba639dc
Uzi-London <75344303+Uzi-London@users.noreply.github.com>
2023-02-07 16:14:25 -0500

    Added initial HotOrCold

---
+++ HotOrCold/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HotOrCold {
+        public class HOC {
+            public static void Main(string[] args) {
+                var random = new Random();
+                int target = random.Next(21); //generate a random number between 0 and 20
+
+                Console.WriteLine(target);
+                
+                int guess;
+                
+                Console.WriteLine("Please guess a number between 0 and 20: ");
+                guess = int.Parse(Console.ReadLine());
+
+                while (guess != target) {
+                    if (guess > target) {
+                        Console.WriteLine("OOPS, That was too high! Try again: ");
+                    }else {
+                        Console.WriteLine("Oh no, too low! Try again: ");
+                    }
+
+                    guess = int.Parse(Console.ReadLine());
+                }
+                Console.WriteLine($"Congratulations, you guessed it! The number was: {target}. Thanks for playing.");     
+        }
+    }
+}
