655dbda254ebd3106e0d350ba12b90e24ba5985c
Uzi-London <75344303+Uzi-London@users.noreply.github.com>
2023-02-07 18:03:13 -0500

    Added code to CodeChallenge 02

--- CodeChallenges/02_DataTypeAndVariables/DataTypeAndVariables/Program.cs
+++ CodeChallenges/02_DataTypeAndVariables/DataTypeAndVariables/Program.cs
@@ -8,23 +8,40 @@ namespace DataTypeAndVariables
       {
           Console.WriteLine("Hello World!");
 
-          byte myByte;
-          sbyte mySbyte;
-          int myInt;
-          uint myUint;
-          short myShort;
-          ushort myUShort;
-          float myFloat;
-          double myDouble;
-          char myCharacter;
-          bool myBool;
-          string myText;
-          string numText;
+          byte myByte = 225;
+          sbyte mySbyte = 52;
+          int myInt = 10;
+          uint myUint = 3;
+          short myShort = 6;
+          ushort myUShort = 8;
+          float myFloat = 15.0f;
+          double myDouble = 20.0;
+          char myCharacter = 'F';
+          bool myBool = true;
+          string myText = "I control text";
+          string numText = "5";
+
+          Console.WriteLine(myByte);
+          Console.WriteLine(mySbyte);
+          Console.WriteLine(myInt);
+          Console.WriteLine(myUint);
+          Console.WriteLine(myShort);
+          Console.WriteLine(myUShort);
+          Console.WriteLine(myFloat);
+          Console.WriteLine(myDouble);
+          Console.WriteLine(myCharacter);
+          Console.WriteLine(myBool);
+          Console.WriteLine(myText);
+          Console.WriteLine(numText);
+          Console.WriteLine(Text2Num(numText));
       }
 
+      //No overload for method
       public static int Text2Num(string numText)
       {
-        throw new NotImplementedException();
+        int tempInt;
+        int.TryParse(numText, out tempInt);
+        return tempInt;
       }
     }
 }
