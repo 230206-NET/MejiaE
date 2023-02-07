dc351bc28ba7dd7bb7629788d4b00474977f6c44
Uzi-London <75344303+Uzi-London@users.noreply.github.com>
2023-02-07 15:29:29 -0500

    Added comments to CoinFlipper

--- CoinFlipper/Program.cs
+++ CoinFlipper/Program.cs
@@ -5,12 +5,12 @@ namespace CoinFlipper {
         public static void Main(string[] args) {
             Console.WriteLine("Coin Flipper starting...");
 
-            string[] coinFaces = {"Heads", "Tails"};
+            string[] coinFaces = {"Heads", "Tails"}; //sets up an array containing the two faces of a coin
 
-            var random = new Random();
-            string result = coinFaces[random.Next(coinFaces.Length)];
+            var random = new Random(); //new Random object which will be used to generate our pseudorandomness
+            string result = coinFaces[random.Next(coinFaces.Length)]; //get a random number between the length of our coinFaces array (between 0 and 1) and use it to get the index value in the array.
 
-            Console.WriteLine($"Your coin was flipped, it was: {result}");
+            Console.WriteLine($"Your coin was flipped, it was: {result}"); //print our result.
         }
     }
 }
