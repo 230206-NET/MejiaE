af465a93c1def95d4f3cf9ab10a47cd685821d11
Uzi-London <75344303+Uzi-London@users.noreply.github.com>
2023-02-07 15:24:42 -0500

    Added initial CoinFlipper

---
+++ CoinFlipper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CoinFlipper {
+    public class Flipper {
+        public static void Main(string[] args) {
+            Console.WriteLine("Coin Flipper starting...");
+
+            string[] coinFaces = {"Heads", "Tails"};
+
+            var random = new Random();
+            string result = coinFaces[random.Next(coinFaces.Length)];
+
+            Console.WriteLine($"Your coin was flipped, it was: {result}");
+        }
+    }
+}
