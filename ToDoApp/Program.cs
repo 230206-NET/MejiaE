b86ae3f4249487fa1bdb2cf9d9d59ae021d4236c
Uzi-London <75344303+Uzi-London@users.noreply.github.com>
2023-02-08 15:52:32 -0500

    Added ToDoApp

---
+++ ToDoApp/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("ToDo App: helping you stay organized.");
+
+List<string> tasks = new List<string>();
+
+while (true) { //infinite loop to continously prompt an user for a task
+    Console.WriteLine("Please input a task and press enter to add it to your 'TO-DO'. Alternatively, you may leave it blank to finish.");
+    string userInput = Console.ReadLine(); //grabbing the user's input
+
+    if (string.IsNullOrEmpty(userInput) || string.IsNullOrWhiteSpace(userInput)) { //if the input is null, empty, or whitespace, then we break out of the loop to stop prompting the user
+        break;
+    } else { //if the input isn't null, whitespace, or empty, then we add it to our list
+        tasks.Add(userInput);
+    } 
+}
+
+Console.WriteLine("Your tasks:");
+
+foreach (string task in tasks) { //loop through the list and print everything  inside
+    Console.WriteLine(task);
+}
